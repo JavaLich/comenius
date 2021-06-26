6f97699eaf414a3c02fd402dff007372947968fb
Abinavraj5427 <abinavraj10@gmail.com>
2021-06-26 11:23:19 -0500

    Added javascript url updates

--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,9 @@
 function setLoginType(type) {
-    if (type === "Learner") {
+    window.location.href = "./login.html?type="+type;
+    if (type === "learner") {
         document.getElementById("learner-selector").classList.add("selected")
         document.getElementById("contributor-selector").classList.remove("selected")
-    } else if (type === "Contributor") {
+    } else if (type === "contributor") {
         document.getElementById("contributor-selector").classList.add("selected")
         document.getElementById("learner-selector").classList.remove("selected")
     }
