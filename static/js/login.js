0867525e83a2cd866f7fdce824f4cd72a8113f87
Abinavraj5427 <abinavraj10@gmail.com>
2021-06-26 11:55:30 -0500

    created post request

--- static/js/login.js
+++ static/js/login.js
@@ -22,4 +22,32 @@ function setLoginType(type) {
     }
 }
 
+function login(){
+    var url_string = window.location.href;
+    var cur_url = new URL(url_string);
+    var type = cur_url.searchParams.get("type");
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    let url = "./login"
+    data = {
+        "username": username,
+        "password": password,
+        "type": type
+    }
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).then(response =>{
+        console.log(response.data)
+    })
+}
+
 initialize();
