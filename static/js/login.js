2f86ff331f4dafec1962f730958c68ceee77b332
Abinavraj5427 <abinavraj10@gmail.com>
2021-06-26 11:27:32 -0500

    Added some javascript logic

--- static/js/login.js
+++ static/js/login.js
@@ -1,3 +1,16 @@
+function initialize(){
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var type = url.searchParams.get("type");
+    if (type === "learner") {
+        document.getElementById("learner-selector").classList.add("selected")
+        document.getElementById("contributor-selector").classList.remove("selected")
+    } else if (type === "contributor") {
+        document.getElementById("contributor-selector").classList.add("selected")
+        document.getElementById("learner-selector").classList.remove("selected")
+    }
+}
+
 function setLoginType(type) {
     window.location.href = "./login.html?type="+type;
     if (type === "learner") {
@@ -8,3 +21,5 @@ function setLoginType(type) {
         document.getElementById("learner-selector").classList.remove("selected")
     }
 }
+
+initialize();
