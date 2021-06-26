4b4289248c658fa67d8c31c9fef5c4f358a371bb
McHippy3 <dwu2010@live.ca>
2021-06-26 05:13:22 -0500

    merge resolve

--- static/js/learner.js
+++ static/js/learner.js
@@ -1,5 +1,10 @@
-function initialize(){
-
-}
-
-initialize();
+document.getElementById("certificate-file-input").addEventListener('change', (event) => {
+    console.log(event.target.value);
+    let fileExtension = event.target.value.split('.').pop();
+    if (!["gif", "jpeg", "jpg", "png", "pdf"].includes(fileExtension)) {
+        event.target.files[0] = null;
+        event.target.value = "";
+        return;
+    }
+    document.getElementById("certificate-file-name-label").innerHTML = event.target.value.split("\\").pop();
+})
